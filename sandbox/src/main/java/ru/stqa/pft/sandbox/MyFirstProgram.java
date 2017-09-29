cd7926054119edee3d871c9665af6a91461f2f0c
GeorgeGeorge90 <31247991+GeorgeGeorge90@users.noreply.github.com>
2017-09-29 17:38:32 +0300

    Фиксинг2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,24 +4,6 @@ import ru.stqa.pft.sandbox.Square;
 
 public class MyFirstProgram {
 
-  public static void main(String[] args) {
-    hello("world");
-    hello("office");
-    hello("neighbor");
-
-    Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
-
-    Rectangle r = new Rectangle(15, 18);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
-  }
-
-  public static void hello(String somebody) {
-    System.out.println("Hello, " + somebody + "!");
-  }
-
-}
-
 
   public static void main(String args[]) {
 
@@ -38,3 +20,8 @@ public class MyFirstProgram {
     System.out.println("p1.distance(60, 80) = " + p1.distance(60, 80));
 
   }
+
+}
+
+
+
